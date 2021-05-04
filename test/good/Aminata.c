fc19aa073162dd1d1cfd18d30b44e98ba88c6541
@Yugo.Cartron <yugo.cartron@insa-rennes.fr>
2021-05-04 16:13:43 +0200

    For et +=

--- test/good/Aminata.c
+++ test/good/Aminata.c
@@ -1,19 +1,15 @@
 /*
 class Aminata {
   public static void main(String[] args) {
-    System.out.println(new A().aminata(10));
+    System.out.println(new A().aminata());
   }
 }
 class A {
-  public int aminata(int n) {
-    int res;
-    if (n == 10 || n == 3) {
-      res = 1;
-    }
-    else {
-      res = 0;
-    }
-    return res;
+  public int aminata() {
+    int i;
+    i = 0;
+    i += 10;
+    return i;
   }
 }*/
 #include <stdio.h>
@@ -22,22 +18,18 @@ class A {
 #pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
 struct array { int* array; int length; };
 struct A;
-void* A_aminata(struct A* this, int n);
+void* A_aminata(struct A* this);
 struct A {
   void* (**vtable)();
 };
 void* (*A_vtable[])() = { A_aminata };
-void* A_aminata(struct A* this, int n) {
-  int res;
-  if (((n == 10) || (n == 3))) {
-    res = 1;
-  }
-  else {
-    res = 0;
-  }
-  return (void*)(res);
+void* A_aminata(struct A* this) {
+  int i;
+  i = 0;
+  i += 10;
+  return (void*)(i);
 }
 int main(int argc, char *argv[]) {
-  printf("%d\n", ({ struct A* tmp1 = ({ struct A* res = calloc(1, sizeof(*res)); res->vtable = A_vtable; res; }); (int) tmp1->vtable[0](tmp1, 10); }));
+  printf("%d\n", ({ struct A* tmp1 = ({ struct A* res = calloc(1, sizeof(*res)); res->vtable = A_vtable; res; }); (int) tmp1->vtable[0](tmp1); }));
   return 0;
 }
